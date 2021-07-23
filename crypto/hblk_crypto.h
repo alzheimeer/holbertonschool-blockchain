dd6ea5f1594ac399f7fe7a86dcb06a331172dc6e
alzheimeer <fogniebla@hotmail.com>
2021-07-22 23:18:48 -0500

    task 6

--- crypto/hblk_crypto.h
+++ crypto/hblk_crypto.h
@@ -28,6 +28,24 @@
 #define PRI_FILENAME   "key.pem"
 #define PUB_FILENAME   "key_pub.pem"
 
+/**
+ * struct sig_s - EC Signature structure
+ *
+ * @sig: Signature buffer. The whole space may not be used
+ * @len: Actual signature size. Can't exceed SIG_MAX_LEN, so stored on a byte
+ * Need in Task 6
+ */
+typedef struct sig_s
+{
+	/*
+	 * @sig must stay first, so we can directly use the structure as
+	 * an array of char
+	 */
+	uint8_t		sig[SIG_MAX_LEN];
+	uint8_t		len;
+} sig_t;
+
+
 uint8_t *sha256(int8_t const *s, size_t len,
 	uint8_t digest[SHA256_DIGEST_LENGTH]);
 EC_KEY *ec_create(void);
@@ -35,5 +53,10 @@ uint8_t *ec_to_pub(EC_KEY const *key, uint8_t pub[EC_PUB_LEN]);
 EC_KEY *ec_from_pub(uint8_t const pub[EC_PUB_LEN]);
 int ec_save(EC_KEY *key, char const *folder);
 EC_KEY *ec_load(char const *folder);
+uint8_t *ec_sign(EC_KEY const *key, uint8_t const *msg,
+		size_t msglen, sig_t *sig);
+
+
+
 
 #endif
