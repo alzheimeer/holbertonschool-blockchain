7dd6f67db25939cdc4acb91fff3c37de1802c09f
alzheimeer <fogniebla@hotmail.com>
2021-07-22 20:48:06 -0500

    fix

--- crypto/hblk_crypto.h
+++ crypto/hblk_crypto.h
@@ -18,11 +18,15 @@
 #include <openssl/ssl.h>
 #include <openssl/pem.h>
 
+/* task0 */
 #define SIG_MAX_LEN    72
+/* task1 */
 #define EC_CURVE   NID_secp256k1
 
+/* task0 */
 uint8_t *sha256(int8_t const *s, size_t len,
 	uint8_t digest[SHA256_DIGEST_LENGTH]);
+/* task1 */	
 EC_KEY *ec_create(void);
 
 #endif
