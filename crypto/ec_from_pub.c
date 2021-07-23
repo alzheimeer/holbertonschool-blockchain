7a65b18e16b34507835626d893aa5600b5d3f744
alzheimeer <fogniebla@hotmail.com>
2021-07-22 23:03:19 -0500

    task 3

---
+++ crypto/ec_from_pub.c
@@ -0,0 +1,36 @@
+#include "hblk_crypto.h"
+
+/**
+ * ec_from_pub - generates EC_KEY from pub key
+ * @pub: the pub key in buffer
+ * Return: pointer to generated EC_KEY struct or NULL
+ */
+EC_KEY *ec_from_pub(uint8_t const pub[EC_PUB_LEN])
+{
+	EC_KEY *key;
+	EC_POINT *point;
+
+	if (!pub)
+		return (NULL);
+	/*Create a key from the curve name*/
+	key = EC_KEY_new_by_curve_name(EC_CURVE);
+	if (!key)
+		return (NULL);
+	/*Create a point*/
+	point = EC_POINT_new(EC_KEY_get0_group(key));
+	if (!point)
+	{
+		EC_KEY_free(key);
+		return (NULL);
+	}
+	/*creates an EC_KEY structure given a public key*/
+	if (!EC_POINT_oct2point(EC_KEY_get0_group(key), point, pub, EC_PUB_LEN, NULL)
+		|| !EC_KEY_set_public_key(key, point))
+	{
+		EC_KEY_free(key);
+		EC_POINT_free(point);
+		return (NULL);
+	}
+	EC_POINT_free(point);
+	return (key);
+}
