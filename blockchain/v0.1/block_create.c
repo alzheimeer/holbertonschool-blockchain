3e86fa0da6f7917a5fac649e75d81a072104bab0
alzheimeer <fogniebla@hotmail.com>
2021-08-03 18:35:07 -0500

    fix task 1

--- blockchain/v0.1/block_create.c
+++ blockchain/v0.1/block_create.c
@@ -10,16 +10,19 @@
 block_t *block_create(block_t const *prev, int8_t const *data,
 	uint32_t data_len)
 {
-	block_t *new_block = calloc(1, sizeof(*new_block));
+	block_t *new_block = NULL;
+	int i = 0;
 
-	if (!new_block)
+	new_block = calloc(1, sizeof(block_t));
+	if (new_block == NULL)
 		return (NULL);
-	(new_block->info).index = (prev->info).index + 1;
-	(new_block->info).difficulty = 0;
-	(new_block->info).timestamp = time(NULL);
-	(new_block->info).nonce = 0;
+	new_block->info.index = (prev->info).index + 1;
+	new_block->info.difficulty = 0;
+	new_block->info.timestamp = time(NULL);
+	new_block->info.nonce = 0;
+	for (i = 0; i < 32; i++)
+		(new_block->info.prev_hash)[i] = (prev->hash)[i];
+	new_block->data.len = SET_MAX_LEN(data_len, BLOCKCHAIN_DATA_MAX);
 	memcpy(new_block->data.buffer, data, new_block->data.len);
-	(new_block->data).len = SET_MAX_LEN(data_len, BLOCKCHAIN_DATA_MAX);
-	memcpy(new_block->info.prev_hash, prev->hash, SHA256_DIGEST_LENGTH);
 	return (new_block);
 }
