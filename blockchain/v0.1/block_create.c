b5a14b783706f018123bcc0e6a095a1090545afe
alzheimeer <fogniebla@hotmail.com>
2021-08-03 14:56:35 -0700

    tash 1

---
+++ blockchain/v0.1/block_create.c
@@ -0,0 +1,25 @@
+#include "blockchain.h"
+
+/**
+ * block_create - creates a new block in the sequence
+ * @prev: pointer to previous block
+ * @data: pointer of data to duplicate
+ * @data_len: length of data
+ * Return: pointer to new block or NULL
+ */
+block_t *block_create(block_t const *prev, int8_t const *data,
+	uint32_t data_len)
+{
+	block_t *new_block = calloc(1, sizeof(*new_block));
+
+	if (!new_block)
+		return (NULL);
+	(new_block->info).index = (prev->info).index + 1;
+	(new_block->info).difficulty = 0;
+	(new_block->info).timestamp = time(NULL);
+	(new_block->info).nonce = 0;
+	memcpy(new_block->data.buffer, data, new_block->data.len);
+	(new_block->data).len = SET_MAX_LEN(data_len, BLOCKCHAIN_DATA_MAX);
+	memcpy(new_block->info.prev_hash, prev->hash, SHA256_DIGEST_LENGTH);
+	return (new_block);
+}
